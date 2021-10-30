6345c9efc9445bd044630dc5c6a3decde2866842
SPANDAN ROY <sproy@microsoft.com>
2021-10-30 20:42:26 +0530

    add comments, add async capability to tests & add code

--- promisePolyfill.js
+++ promisePolyfill.js
@@ -11,6 +11,10 @@ const states = {
  * A basic polyfill of JS promise.
  */
 class promisePolyfill {
+  /**
+   * Promise's executor function.
+   * @param {*} executor Contructor function of promise.
+   */
   constructor(executor) {
     /**
      * Set the initial state to pending until the promise is settled- fulfilled/rejected.
@@ -36,10 +40,16 @@ class promisePolyfill {
     setTimeout(() => {
       try {
         executor(this._resolve.bind(this), this._reject.bind(this));
-      } catch (exception) {}
+      } catch (error) {
+        this._reject(error);
+      }
     });
   }
 
+  /**
+   * Implementation of resolution function.
+   * @param {*} value Value with which we resolve promise.
+   */
   _resolve(value) {
     if (this.state === states.PENDING) {
       this._value = value;
@@ -52,13 +62,20 @@ class promisePolyfill {
     while (this.thenQ.length > 0) {
       const { resolutionHandler, promise } = this.thenQ.shift();
       if (typeof resolutionHandler === "function") {
-        const rV = resolutionHandler(this._value);
+        let rV;
+        try {
+          rV = resolutionHandler(this._value);
+        } catch (error) {
+          promise._reject(error);
+        }
         /**
          * Check if return value is a promise (thenable).
          */
         if (rV && typeof rV.then === "function") {
           rV.then((value) => {
             promise._resolve(value);
+          }).catch((reason) => {
+            promise._reject(reason);
           });
         } else {
           promise._resolve(rV);
@@ -72,6 +89,11 @@ class promisePolyfill {
     }
   }
 
+  /**
+   * Implementation of then handler.
+   * @param {*} resolutionHandler The callback to be executed when promise is resolved.
+   * @returns A new promise which can be used for chaining.
+   */
   then(resolutionHandler) {
     const promise = new promisePolyfill(() => {});
     this.thenQ.push({ resolutionHandler, promise });
@@ -83,6 +105,10 @@ class promisePolyfill {
     return promise;
   }
 
+  /**
+   * Implementation of rejection function.
+   * @param {*} reason Reason with which we reject promise.
+   */
   _reject(reason) {
     if (this.state === states.PENDING) {
       this._reason = reason;
@@ -102,7 +128,6 @@ class promisePolyfill {
   runRejectionHandler() {
     while (this.catchQ.length > 0) {
       const { rejectionHandler, promise } = this.catchQ.shift();
-
       if (typeof rejectionHandler === "function") {
         const rV = rejectionHandler(this._reason);
         /**
@@ -124,6 +149,11 @@ class promisePolyfill {
     }
   }
 
+  /**
+   * Implementation of catch handler.
+   * @param {*} rejectionHandler The callback to be executed when promise is rejected.
+   * @returns A new promise which can be used for chaining.
+   */
   catch(rejectionHandler) {
     const promise = new promisePolyfill(() => {});
     this.catchQ.push({ rejectionHandler, promise });
--- promisePolyfill.test.js
+++ promisePolyfill.test.js
@@ -1,109 +1,291 @@
 const promisePolyfill = require("./promisePolyfill");
 
 describe("Then handler of promise should work properly", () => {
-  test("Resolving promise with value passes the resolution value to its then-handler", () => {
+  test("Resolving promise with value passes the resolution value to its then-handler", (done) => {
     const promise = new promisePolyfill((resolve, _reject) => {
       resolve(9);
     });
     promise.then((value) => {
-      expect(value).toBe(9);
+      try {
+        expect(value).toBe(9);
+        done();
+      } catch (error) {
+        done(error);
+      }
     });
   });
 
-  test("Passing a non-function value to then-handler should result in then-handler getting resolved with value of its parent promise", () => {
-    const promise = new promisePolyfill((res) => {
+  test("Passing a non-function value to then-handler should result in then-handler getting resolved with value of its parent promise", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve(18);
+    });
+    promise
+      .then(
+        "I'm a non-function value. Ideally, I should be resolved with value of my parent promise"
+      )
+      .then((value) => {
+        try {
+          expect(value).toBe(18);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+  });
+
+  test("Chaining works with then-handlers", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
       resolve(9);
     });
     promise
-      .then("I am a non-function handler. Ideally, I should be skipped")
       .then((value) => {
-        expect(value).toBe(9);
-      });
-  });
-
-  test("Chaining works with then-handlers", () => {
-    const promise = new promisePolyfill((res) => {
-      res(9);
-    });
-    promise
-      .then((value) => {
-        expect(value).toBe(9);
+        try {
+          expect(value).toBe(9);
+        } catch (error) {
+          done(error);
+        }
         return value + 1;
       })
       .then((value) => {
-        expect(value).toBe(10);
+        try {
+          expect(value).toBe(10);
+        } catch (error) {
+          done(error);
+        }
         return value + 2;
       })
       .then((value) => {
-        expect(value).toBe(12);
+        try {
+          expect(value).toBe(12);
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
   });
 
-  test("Promise should support multiple then handlers", () => {
-    const promise = new promisePolyfill((res) => {
-      res(9);
+  test("Promise should support multiple then handlers", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve(9);
     });
     promise
       .then((value) => {
-        expect(value).toBe(9);
+        try {
+          expect(value).toBe(9);
+        } catch (error) {
+          done(error);
+        }
         return value + 1;
       })
       .then((value) => {
-        expect(value).toBe(10);
+        try {
+          expect(value).toBe(10);
+        } catch (error) {
+          done(error);
+        }
       });
 
     promise.then((value) => {
-      expect(value).toBe(9);
+      try {
+        expect(value).toBe(9);
+        done();
+      } catch (error) {
+        done(error);
+      }
     });
   });
 
-  test("Resolution handler works when return value is promise", () => {
-    const promise = new promisePolyfill((res) => {
-      res(9);
+  test("Resolution handler works when return value is promise", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve(9);
     });
 
     promise
       .then((value) => {
-        expect(value).toBe(9);
+        try {
+          expect(value).toBe(9);
+        } catch (error) {
+          done(error);
+        }
         return value + 1;
       })
       .then((value) => {
-        expect(value).toBe(10);
-        return new promisePolyfill((res) => {
-          res(value + 2);
+        try {
+          expect(value).toBe(10);
+        } catch (error) {
+          done(error);
+        }
+        return new promisePolyfill((resolve, _reject) => {
+          resolve(value + 2);
         });
       })
       .then((value) => {
-        expect(value).toBe(12);
+        try {
+          expect(value).toBe(12);
+          done();
+        } catch (error) {
+          done(error);
+        }
       });
   });
 
-  test("Once promise is resolved, resolving it again with another value has no effect", () => {
-    const promise = new promisePolyfill((res) => {
-      res(9);
-      res(18);
+  test("Once promise is resolved, resolving it again with another value has no effect", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve(9);
+      resolve(18);
     });
     promise.then((value) => {
-      expect(value).toBe(9);
+      try {
+        expect(value).toBe(9);
+      } catch (error) {
+        done(error);
+      }
       setTimeout(() => {
         promise.then((value) => {
-          expect(value).toBe(9);
+          try {
+            expect(value).toBe(9);
+            done();
+          } catch (error) {
+            done(error);
+          }
         });
       });
     });
   });
 
-  test("Promise can be resolved again once it has been resolved", () => {
-    const promise = new promisePolyfill((res) => {
-      res(9);
+  test("Promise can be resolved again once it has been resolved", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve(9);
     });
     promise.then((value) => {
-      expect(value).toBe(9);
+      try {
+        expect(value).toBe(9);
+      } catch (error) {
+        done(error);
+      }
       setTimeout(() => {
         promise.then((value) => {
-          expect(value).toBe(9);
+          try {
+            expect(value).toBe(9);
+            done();
+          } catch (error) {
+            done(error);
+          }
         });
       });
     });
+  });
+});
+
+describe("Catch handler of promise should work properly", () => {
+  test("Rejecting promise with reason passed rejection reason to its catch-handler", (done) => {
+    const promise = new promisePolyfill((_resolve, reject) => {
+      reject("Error");
+    });
+    promise.catch((reason) => {
+      try {
+        expect(reason).toBe("Error");
+      } catch (error) {
+        done(error);
+      }
+      done();
+    });
+  });
+
+  test("Passing a non-function value to catch-handler should result in catch-handler getting reject with reason of its parent promise", (done) => {
+    const promise = new promisePolyfill((_resolve, reject) => {
+      reject("Error");
+    });
+    promise
+      .catch(
+        "I'm a non-function value. Ideally, I should be rejected with value of my parent promise"
+      )
+      .catch((reason) => {
+        try {
+          expect(reason).toBe("Error");
+        } catch (error) {
+          done(error);
+        }
+        done();
+      });
+  });
+
+  test("Chaining should work with catch-handlers", (done) => {
+    const promise = new promisePolyfill((_resolve, reject) => {
+      reject("Something went wrong");
+    });
+    promise
+      .then((value) => {
+        return value;
+      })
+      .catch((reason) => {
+        try {
+          expect(reason).toBe("Something went wrong");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+  });
+
+  test("Promises that are rejected within then-handler should be cuaght in catch block", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve(27);
+    });
+    promise
+      .then((value) => {
+        try {
+          expect(value).toBe(27);
+        } catch (error) {
+          done(error);
+        }
+        return new promisePolyfill((_resolve, reject) => {
+          reject("Error");
+        });
+      })
+      .catch((reason) => {
+        try {
+          expect(reason).toBe("Error");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+  });
+
+  test("Catch-handler should catch error thrown in then-handler", (done) => {
+    const promise = new promisePolyfill((resolve, _reject) => {
+      resolve();
+    });
+    promise
+      .then(() => {
+        throw new Error("Something went wrong");
+      })
+      .catch((reason) => {
+        try {
+          expect(reason.message).toBe("Something went wrong");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+  });
+
+  test("Catch-handler should catch error thrown in executor function", (done) => {
+    const promise = new promisePolyfill(() => {
+      throw new Error("Something went wrong");
+    });
+    promise
+      .then(() => {
+        return "There goes nothing!";
+      })
+      .catch((reason) => {
+        try {
+          expect(reason.message).toBe("Something went wrong");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
   });
 });
